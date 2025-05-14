38bf0431f0a0322d61fba63345a6ccdd08407c06
Luke <luke.bobinski@protonmail.com>
2025-05-14 19:17:51 +0200

    v1

--- html/autorzy.php
+++ html/autorzy.php
@@ -6,21 +6,21 @@
     <div class="col-md-8">
         <div class="card mb-4">
             <div class="card-body">
-                <h5 class="card-title">Łukasz Bobiński</h5>
+                <h5 class="card-title">Autor 1</h5>
                 <p class="card-text">Odpowiada za strukturę aplikacji, testy i integrację modułów.</p>
             </div>
         </div>
 
         <div class="card mb-4">
             <div class="card-body">
-                <h5 class="card-title">Monika Kowalska</h5>
+                <h5 class="card-title">Autor 2</h5>
                 <p class="card-text">Projektantka interfejsu użytkownika oraz odpowiedzialna za front-end w Bootstrap.</p>
             </div>
         </div>
 
         <div class="card mb-4">
             <div class="card-body">
-                <h5 class="card-title">Tomasz Nowak</h5>
+                <h5 class="card-title">Autor 3</h5>
                 <p class="card-text">Autor logiki obliczeniowej w PHP, implementacja kalkulatorów i obsługi danych wejściowych.</p>
             </div>
         </div>
